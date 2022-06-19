ab19fab895a65684cf2d5b89f54a218d46fa0aed
RnD.Askella <dev_rnd.askella@yahoo.com>
2022-06-19 11:58:04 +0300

    Create User model

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  emal: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  diskSpace: { type: Number, default: 1024 ** 3 * 10 },
+  useSpace: { type: Number, default: 0 },
+  avatar: { type: String },
+  files: [{ type: Schema.Types.ObjectId, ref: "files" }],
+});
+
+module.exports = model("users", userSchema);
