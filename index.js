8b6e51351e2376c191b7d63a034512a3576ccf80
RnD.Askella <dev_rnd.askella@yahoo.com>
2022-06-19 11:25:39 +0300

    init server and start db

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+// ====== LIB ======
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
+// ====== INNER IMPORTS ======
+
+// ====== CONFIGURATION ======
+
+const app = express();
+const SERVER_PORT = config.get("server.port");
+const DB_URL = `${config.get("db.host")}:${config.get("db.port")}/${config.get(
+  "db.name"
+)}`;
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(DB_URL);
+
+    app.listen(SERVER_PORT, () => console.log("Server starting ..."));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+startServer();
+
+// ====== ROUTES ======
